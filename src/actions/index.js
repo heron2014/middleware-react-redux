df0be35b8e38428b78edb84cd69b514132346933
Anita <a.nita@hotmail.co.uk>
2016-09-16 13:44:45 +0100

    added userList connected component and show dummy data

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+import { FETCH_USERS } from './types';
+
+export function fetchUsers() {
+  return {
+    type: FETCH_USERS,
+    payload: [
+      {name: 'Anita'}, {name: 'Alex'}, {name: 'Andrew'}
+    ]
+  };
+}
